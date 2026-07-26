2bc7aef3ef9247a4ce98ab7663bee9c6794fb906
TeaChoco <thannateetibmerit@gmail.com>
2026-07-26 15:57:52 +0700

    first commit

---
+++ src-tauri/gen/android/app/src/main/java/com/teachoco/tauri_vite_react_typescript/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.teachoco.tauri_vite_react_typescript
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+
+class MainActivity : TauriActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    enableEdgeToEdge()
+    super.onCreate(savedInstanceState)
+  }
+}
